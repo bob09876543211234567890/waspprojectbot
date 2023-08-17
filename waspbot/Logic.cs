399f21e10ea5c2791b763b69f23bea38084c4ff0
ТАТЫЭНА <tshatkov4@gmail.com>
2023-08-17 13:19:41 +0300

    Добавьте файлы проекта.

---
+++ waspbot/Logic.cs
@@ -0,0 +1,17 @@
+﻿using Telegram;
+using System;
+using Telegram.Bot;
+
+
+namespace ConsoleApp1
+{
+    public class Logic
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
+
+
